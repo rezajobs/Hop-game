701a6561dc1d8b62be013d61b647fc35799c4708
rezajobs <120446719+rezajobs@users.noreply.github.com>
2024-05-19 01:03:55 -0400

    Update p1= 'player1'.py
    new

--- p1= 'player1'.py
+++ p1= 'player1'.py
@@ -1,46 +1,55 @@
 player1 = 'Player 1'
 player2 = 'Player 2'
-counter = 1
+level = 1
 is_running = True
 
-score1= 3
-score2= 3
-
-player1_first_turn = True
+score1 = 3
+score2 = 3
 
 while is_running:
-    player = player1 if counter % 2 != 0 else player2
-      
-    while True:
-        user_input= input(f'{player}:')
-        if player == player1 and player1_first_turn:
-            if user_input.startswith('1'):
-                player1_first_turn = False
-                break
-            else:
-                print("Player 1's first input must start with 1. Try again.")
-        else:
-            break
-
-    if counter %5 == 0:
-        if  user_input.lower == 'hop':
+    player = player1 if level % 2 != 0 else player2
+    
+    user_input = input(f'{player}: ')
+    
+    if level % 5 == 0:
+        if user_input.lower() == 'hop':
             print(user_input)
         else:
-            if  counter % 2 == 0:
-                score2 -= 1
-                print(f'{player},new score is {score2}')
+            print('error')
+            if player == player1:
+                score1 -= 1
+                print(f'{player}, new score is {score1}')
             else:
-                score1-=1
-                print(f'{player},new score is {score1}')
-
+                score2 -= 1
+                print(f'{player}, new score is {score2}')
+            
             if score1 == 0:
                 print(f'{player1} loses')
+                is_running = False
                 break
             elif score2 == 0:
-                print(f"{player2} loses")
+                print(f'{player2} loses')
+                is_running = False
                 break
     else:
-        print(user_input)
-        
+        if str(level) == user_input:
+            print(user_input)
+        else:
+            print('error')
+            if player == player1:
+                score1 -= 1
+                print(f'{player}, new score is {score1}')
+            else:
+                score2 -= 1
+                print(f'{player}, new score is {score2}')
+            
+            if score1 == 0:
+                print(f'{player1} loses')
+                is_running = False
+                break
+            elif score2 == 0:
+                print(f'{player2} loses')
+                is_running = False
+                break
 
-    counter += 1
+    level += 1
